0f550f054f1abb74195f88bff5518347a6708d91
Reddy531 <chakradhar.lsg4@gmail.com>
2021-10-19 19:06:23 -0500

    Updated with getCustomerByAddress

--- src/main/java/com/chakradhar/graphql/query/CustomerQuery.java
+++ src/main/java/com/chakradhar/graphql/query/CustomerQuery.java
@@ -14,6 +14,10 @@ public class CustomerQuery implements GraphQLQueryResolver {
     @Autowired
     private CustomerService customerService;
 
+    public List<CustomerObj> customerDetails() {
+        return this.customerService.getCustomerDetails();
+    }
+
     public List<CustomerObj> getCustomers(final int count) {
         return this.customerService.getAllCustomers(count);
     }
@@ -21,4 +25,10 @@ public class CustomerQuery implements GraphQLQueryResolver {
     public CustomerObj customerbyId(final String customer_id) {
         return this.customerService.getCustomerbyId(customer_id);
     }
+
+
+//customersByAddress(addressFilter: AddressFilter): [CustomerObj]
+//    public List<CustomerObj> customersByAddress() {
+//        return this.customerService.getCustomersByAddress();
+//    }
 }
--- src/main/java/com/chakradhar/graphql/service/CustomerService.java
+++ src/main/java/com/chakradhar/graphql/service/CustomerService.java
@@ -27,52 +27,17 @@ public class CustomerService {
     }
 
     /**
-     * If postgres db is not connected
-     */
-    /*
-    @Transactional
-    public CustomerCreated createCustomer(final String customer_id, final String first_name, final String last_name, final String email_id, final long phone_number, final String creation_date) {
-        final Customer customer = new Customer();
-        customer.setCustomer_id(customer_id);
-        customer.setFirst_name(first_name);
-        customer.setLast_name(last_name);
-        customer.setEmail_id(email_id);
-        customer.setPhone_number(phone_number);
-        customer.setCreation_date(creation_date);
-        return new CustomerCreated(this.customerRepository.save(customer).getCustomer_id());
-    }
-
-    @Transactional(readOnly = true)
-    public List<Customer> getAllCustomers(final int count) {
-        return this.customerRepository.findAll().stream().limit(count).collect(Collectors.toList());
-    }
-
-    @Transactional(readOnly = true)
-    public Customer getCustomerbyId(final String id) {
-        //TODO: need to add persistant logic to get customer by ID
-        return null;
-    }
-    */
-
-    /**
      * If postgres db is connected
      */
 
-    @Transactional
-    public CustomerCreated createCustomer(final String customer_id, final String first_name, final String last_name, final String email_id, final long phone_number, final String creation_date) {
-        final Customer customer = new Customer();
-        customer.setCustomer_id(customer_id);
-        customer.setFirst_name(first_name);
-        customer.setLast_name(last_name);
-        customer.setEmail_id(email_id);
-        customer.setPhone_number(phone_number);
-        customer.setCreation_date(creation_date);
-        int i = jdbcTemplate.update("INSERT INTO customers (customer_id, first_name, last_name, email_id, phone_number, creation_date) VALUES(?,?,?,?,?,?)",
-                new Object[]{customer.getCustomer_id(), customer.getFirst_name(), customer.getLast_name(), customer.getEmail_id(), customer.getPhone_number(), customer.getCreation_date()});
-        if (i != 0)
-            return new CustomerCreated(customer.getCustomer_id());
-        else
-            return new CustomerCreated();
+    @Transactional(readOnly = true)
+    public List<CustomerObj> getCustomerDetails() {
+        List<CustomerObj> customerObjs = jdbcTemplate.query("SELECT * from customers", BeanPropertyRowMapper.newInstance(CustomerObj.class));
+
+        for (CustomerObj customerObj : customerObjs) {
+            customerObj.setAddressDetails(getAddessbyCustomerId(customerObj.getCustomer_id()));
+        }
+        return customerObjs;
     }
 
     @Transactional(readOnly = true)
@@ -101,4 +66,54 @@ public class CustomerService {
             return null;
         }
     }
+
+//    @Transactional(readOnly = true)
+//    public CustomerObj getCustomersByAddress() {
+//        return new CustomerObj();
+//    }
+
+    @Transactional
+    public CustomerCreated createCustomer(final String customer_id, final String first_name, final String last_name, final String email_id, final long phone_number, final String creation_date) {
+        final Customer customer = new Customer();
+        customer.setCustomer_id(customer_id);
+        customer.setFirst_name(first_name);
+        customer.setLast_name(last_name);
+        customer.setEmail_id(email_id);
+        customer.setPhone_number(phone_number);
+        customer.setCreation_date(creation_date);
+        int i = jdbcTemplate.update("INSERT INTO customers (customer_id, first_name, last_name, email_id, phone_number, creation_date) VALUES(?,?,?,?,?,?)",
+                new Object[]{customer.getCustomer_id(), customer.getFirst_name(), customer.getLast_name(), customer.getEmail_id(), customer.getPhone_number(), customer.getCreation_date()});
+        if (i != 0)
+            return new CustomerCreated(customer.getCustomer_id());
+        else
+            return new CustomerCreated();
+    }
+
+    /**
+     * If postgres db is not connected
+     */
+    /*
+    @Transactional
+    public CustomerCreated createCustomer(final String customer_id, final String first_name, final String last_name, final String email_id, final long phone_number, final String creation_date) {
+        final Customer customer = new Customer();
+        customer.setCustomer_id(customer_id);
+        customer.setFirst_name(first_name);
+        customer.setLast_name(last_name);
+        customer.setEmail_id(email_id);
+        customer.setPhone_number(phone_number);
+        customer.setCreation_date(creation_date);
+        return new CustomerCreated(this.customerRepository.save(customer).getCustomer_id());
+    }
+
+    @Transactional(readOnly = true)
+    public List<Customer> getAllCustomers(final int count) {
+        return this.customerRepository.findAll().stream().limit(count).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Customer getCustomerbyId(final String id) {
+        //TODO: need to add persistant logic to get customer by ID
+        return null;
+    }
+    */
 }
